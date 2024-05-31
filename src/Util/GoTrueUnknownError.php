98fbb14d2a3fc9cd55bca462396520aa2ef75cd6
Spelunky <myCodebox@users.noreply.github.com>
2024-05-31 12:52:35 +0200

    Fix a wrong type

--- src/Util/GoTrueUnknownError.php
+++ src/Util/GoTrueUnknownError.php
@@ -5,7 +5,7 @@ namespace Supabase\Util;
 class GoTrueUnknownError extends GoTrueError
 {
 	private string $name = 'GoTrueUnknownError';
-	private mixed $originalError = null;
+	private $originalError = null;
 
 	public function __construct($message, $originalError)
 	{
